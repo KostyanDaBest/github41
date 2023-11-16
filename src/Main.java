51e4d3efc298e4fb36f1866fa5258662de0b9ed1
kosty <round.mc@yandex.by>
2023-11-16 12:59:34 -0500

    new

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+ Random random = new Random();
+        int arr [] = new int[9];
+        System.out.print(" Правильный  порядок: " );
+        for (int i = 0;  i < 9; i++) {
+            System.out.print( i + " ");
+        }
+        System.out.print( ";");
+        System.out.print( " Обратный порядок: " + " " );
+        for (int i =arr.length-1 ; i>=0 ; i--) {
+
+            System.out.print( i+ " " );
+        }
+        System.out.println(".");
+    }
 }
